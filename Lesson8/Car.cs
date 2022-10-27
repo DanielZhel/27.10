04067c6fcc97771175c79260e8c213cd6e6932df
Danielzhel <fartunazh@gmail.com>
2022-10-27 21:05:01 +0300

    Добавьте файлы проекта.

---
+++ Lesson8/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public abstract class Car : IVIhicle
+    {
+       
+        public Car(int fuel, int cons)
+        {
+            cons = ;
+            fuel = ;
+            return;
+
+
+        }
+
+        public void Drive(int distance)
+        {
+            Console.WriteLine("Enter amount of the fuel");
+            
+            int amount = Convert.ToInt16(Console.ReadLine());
+            if (Refuel(amount) == true)
+            {
+                Console.WriteLine("The car is driving now");
+            }
+            else
+                Console.WriteLine("No fuel");
+            
+
+        }
+        public bool Refuel(int amount)
+        {
+            bool refuel = true;
+            if (amount > 0)
+            {
+               refuel = true;
+            }
+            return refuel;
+
+        }
+
+    }
+}
+   
+
---
+++ Lesson8/Interface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public interface IVIhicle
+    {
+        void Drive(int distance);
+
+        bool Refuel(int amount);
+    }
+}
